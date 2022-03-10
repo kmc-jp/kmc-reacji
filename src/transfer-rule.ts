4fbaa3cf796e75d2e9b1116a5fc337ae43139433
segre <segrehouck5458@gmail.com>
2022-03-10 20:40:49 +0900

    add segre id

--- src/transfer-rule.ts
+++ src/transfer-rule.ts
@@ -2,4 +2,5 @@ export const rule: { [key: string]: string } = {
   // [ID]: [転送先のチャンネルID]
   U01U7S3UFAB: "C0373NXS7ME", // ryokohbato
   U01TYAVASG4: "C03668M4729",
+  U01T8E7LQ4F: "C036E0WDJFL", // segre
 };
