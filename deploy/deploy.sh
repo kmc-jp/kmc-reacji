15a818cb83fccc836f7813e9c91c58f037615489
ryokohbato <ryokohbato@gmail.com>
2022-04-07 14:00:17 +0900

    終了コードを上書き

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,4 +4,4 @@ rsync -acvz $(pwd)/dist/kmc-reacji.js ec2-user@miyama:/home/ec2-user/.local/bin/
 
 # サーバー再起動
 ssh ec2-user@miyama "kill $(ssh ec2-user@miyama "ps aux | grep kmc-reacji" | grep node | awk '{ print $2 }')"
-timeout 3 ssh ec2-user@miyama "nohup node ~/.local/bin/kmc-reacji/kmc-reacji.js &" 
+timeout 3 ssh ec2-user@miyama "nohup node ~/.local/bin/kmc-reacji/kmc-reacji.js &" || exit 0
